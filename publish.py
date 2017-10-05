bffa3651f65d3cd447244eeefa3a50288d434c29
rendaw <spoo@zarbosoft.com>
2017-10-05 16:18:18 +0900

    Pub script fix

--- publish.py
+++ publish.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-from subprocess import check_call as cc
+import subprocess
 import argparse
 import re
 from os.path import exists
 
 parser = argparse.ArgumentParser()
 parser.add_argument('version')
+parser.add_argument('-f', '--force', action='store_true')
 args = parser.parse_args()
 if not args.force and subprocess.call(['git', 'diff-index', '--quiet', 'HEAD']) != 0:  # noqa
     raise RuntimeError('Working directory must be clean.')
 if not re.match('\\d+\\.\\d+\\.\\d+', args.version):
     args.error('version must be in the format N.N.N')
-cc(['python', 'setup.py', 'sdist'])
+subprocess.check_call(['python', 'setup.py', 'sdist'])
 dist = 'dist/depflow-{}.tar.gz'.format(args.version)
 if not exists(dist):
     args.error('Deploy version doesn\'t match source version.')
-cc(['twine', 'upload', dist, '--user', 'rendaw'])
+subprocess.check_call(['twine', 'upload', dist, '--user', 'rendaw'])
