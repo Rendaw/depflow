e395621e21da1221b64089de5c1c39e5a40252c1
rendaw <spoo@zarbosoft.com>
2017-10-05 16:01:53 +0900

    Versioning tools

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(
     name='depflow',
     version='0.0.1',
-    author='Andrew Baxter',
-    author_email='afb2@wustl.edu',
-    url='https://github.com/andrewbaxter/depflow',
+    author='rendaw',
+    url='https://github.com/rendaw/depflow',
     license='BSD',
     py_modules=['depflow'],
 )
---
+++ version.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument('version')
+parser.add_argument('-f', '--force', action='store_true')
+args = parser.parse_args()
+if not args.force and subprocess.call(['git', 'diff-index', '--quiet', 'HEAD']) != 0:  # noqa
+    raise RuntimeError('Working directory must be clean.')
+if not re.match('\\d+\\.\\d+\\.\\d+', args.version):
+    args.error('version must be in the format N.N.N')
+subprocess.check_call([
+    'sed',
+    '-e', 's/\\(version=\'\\)[[:digit:]]\\+\\.[[:digit:]]\\+\\.[[:digit:]]\\+\\(\'\\)/\\1{}\2/g'.format(  # noqa
+        args.version
+    ),
+    '-i',
+    'setup.py',
+])
+subprocess.call([
+    'git',
+    'commit',
+    '-a',
+    '-m', 'VERSION {}'.format(args.version),
+])
+subprocess.check_call([
+    'git',
+    'tag',
+    '-a', 'v{}'.format(args.version),
+    '-m', 'v{}'.format(args.version),
+    '-f',
+])
+subprocess.check_call([
+    'git',
+    'push',
+])
+subprocess.check_call([
+    'git',
+    'push',
+    '--tags',
+    '-f',
+])
