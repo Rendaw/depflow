21911b9c48dab0a46dde033af3e9c0e33b051bd1
rendaw <spoo@zarbosoft.com>
2017-10-05 16:13:27 +0900

    VERSION 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='depflow',
-    version='0.0.1',
+    version='0.0.2,
     author='rendaw',
     url='https://github.com/rendaw/depflow',
     license='BSD',
